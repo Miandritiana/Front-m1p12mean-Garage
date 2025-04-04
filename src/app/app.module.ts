3277887eca8c2099e2c28750a20af7719e2eef27
Henintsooa <120570194+Henintsooa@users.noreply.github.com>
2025-04-05 00:30:45 +0300

    suppression static.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,17 @@ import { AcceuilModule } from './pages/acceuil/acceuil.module';
 import { DemandePrestationComponent } from './pages/demande-prestation/demande-prestation/demande-prestation.component';
 import { DemandePrestationManagerComponent } from './pages/demande-prestation-manager/demande-prestation-manager/demande-prestation-manager.component';
 import { RouterModule } from '@angular/router';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 import { MecanicienModule } from './pages/mecanicien/mecanicien.module';
 @NgModule({
   imports: [
     AcceuilModule
   ],
-  declarations: [
+  declarations : [
     DemandePrestationComponent,
     DemandePrestationManagerComponent,
     RouterModule,
     MecanicienModule
-  ],
-  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
+  ]
 })
 export class AppModule { }
