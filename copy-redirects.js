0eb14df179a443116687df1b3f4b10ebd61d6990
Henintsooa <120570194+Henintsooa@users.noreply.github.com>
2025-04-05 00:38:00 +0300

    Update copy-redirects.js

--- copy-redirects.js
+++ copy-redirects.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const srcPath = path.join(__dirname, 'src', '_redirects');
-const destPath = path.join(__dirname, 'dist', 'coreui-free-angular-admin-template', '_redirects');
+const destPath = path.join(__dirname, 'dist', 'coreui-free-angular-admin-template', 'browser', '_redirects');
 
 fs.copyFileSync(srcPath, destPath);
 console.log('✅ _redirects file copied to dist folder');
