f447c21f0bcda868c9758256b1cb57e82805e5e9
Miandritiana <120574096+Miandritiana@users.noreply.github.com>
2025-04-04 15:24:18 +0300

    remove unuse module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { AcceuilModule } from './pages/acceuil/acceuil.module';
-import { NgChartsModule } from 'ng2-charts';
 import { DemandePrestationComponent } from './pages/demande-prestation/demande-prestation/demande-prestation.component';
 import { DemandePrestationManagerComponent } from './pages/demande-prestation-manager/demande-prestation-manager/demande-prestation-manager.component';
 import { RouterModule } from '@angular/router';
@@ -8,7 +7,6 @@ import { RouterModule } from '@angular/router';
 import { MecanicienModule } from './pages/mecanicien/mecanicien.module';
 @NgModule({
   imports: [
-    NgChartsModule,
     AcceuilModule
   ],
   declarations : [
